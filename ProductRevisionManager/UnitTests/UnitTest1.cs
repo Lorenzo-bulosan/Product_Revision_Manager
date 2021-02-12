67871b5de3357d5c6677a1f54da75fef02bf0d09
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2021-02-12 03:16:14 +0000

    added more unit test

--- ProductRevisionManager/UnitTests/UnitTest1.cs
+++ ProductRevisionManager/UnitTests/UnitTest1.cs
@@ -172,6 +172,49 @@ namespace UnitTests
             Assert.AreEqual(resultTitle, correctTitle);
         }
 
+        [Test]
+        public void WhenAddingCommentToTaskCountByOne()
+        {
+            // generate revision first and get id
+            _revisionManager.GenerateRevisionForProjectID(_projectIdToTest, DateTime.Now);
+            Dictionary<int, DateTime> revisionInfo = (Dictionary<int, DateTime>)_revisionManager.GetRevisionsFromProject(_projectIdToTest);
+            int revisionId = revisionInfo.FirstOrDefault().Key;
+
+            // generate task for testing
+            _revisionManager.AddTaskToRevision(revisionId, "title", "description", 0, 1, "google");
+            int taskID = _revisionManager.GetTasksFromRevisionID(revisionId).FirstOrDefault().TaskID;
+
+            // add comment and count before and after
+            using (var db = new MonokayuDbContext())
+            {
+                var countBefore = db.TaskComments.Where(c => c.TaskID == taskID).Count();                
+                _revisionManager.AddCommentToTaskID(taskID, "Test comment", "LB");
+                var countAfter = db.TaskComments.Where(c => c.TaskID == taskID).Count();
+
+                Assert.AreEqual(countAfter, countBefore+1);
+            }
+        }
+
+        [Test]
+        public void WhenRetrievingACommentMakeReturnsCorrectInformation()
+        {
+            // generate revision first and get id
+            _revisionManager.GenerateRevisionForProjectID(_projectIdToTest, DateTime.Now);
+            Dictionary<int, DateTime> revisionInfo = (Dictionary<int, DateTime>)_revisionManager.GetRevisionsFromProject(_projectIdToTest);
+            int revisionId = revisionInfo.FirstOrDefault().Key;
+
+            // generate task for testing
+            _revisionManager.AddTaskToRevision(revisionId, "title", "description", 0, 1, "google");
+            int taskID = _revisionManager.GetTasksFromRevisionID(revisionId).FirstOrDefault().TaskID;
+
+            // add comment to test
+            _revisionManager.AddCommentToTaskID(taskID, "Test comment", "LB");
+
+            // query comment
+            var comment = _revisionManager.RetrieveCommentsFromTaskID(taskID).FirstOrDefault();
+            Assert.AreEqual(comment.comment, "Test comment");
+        }
+
         [TearDown]
         public void TearDown()
         {
@@ -188,106 +231,5 @@ namespace UnitTests
                 db.SaveChanges();
             }
         }
-
-        //[TestCase(5, "Testing title", "Description test", 3, 0, "empty url")]
-        //[TestCase(6, "in revision 6", "Description test", 3, 0, "empty url")]
-        //[TestCase(7, "in revision 7", "Description test", 3, 0, "empty url")]
-        //public void AddingAtaskToArevisionIncreasesNumberOfTasksByOne(int revisionID, string title, string description, int urgency, int progress, string url)
-        //{
-
-        //    // known project id, also that this will result in revision id 5
-        //    //GenerateRevisionForProjectID(9);
-
-        //    // testing
-        //    using (var db = new MonokayuDbContext())
-        //    {
-        //        var numberOfTasksBefore = db.RevisionTasks.Count();
-        //        _revisionManager.AddTaskToRevision(revisionID, title, description, urgency, progress, url);
-        //        var numberOfTasksAfter = db.RevisionTasks.Count();
-
-        //        Assert.AreEqual(numberOfTasksBefore + 1, numberOfTasksAfter);
-        //    }
-        //}
-
-        //[TestCase(14, "UNCHANGED", "description", 1, 1, "url", "CHANGED", "CHANGED")]
-        //public void UpdatingTaskRowsCorrectly(int taskID, string title, string description, int urgency, int progress, string url, string knownAnswer, string editTitle)
-        //{
-
-        //    // update
-        //    _revisionManager.UpdateRevisionTask(taskID, editTitle, description, urgency, progress, url);
-
-        //    // testing
-        //    using (var db = new MonokayuDbContext())
-        //    {
-        //        var information = db.RevisionTasks.Where(r => r.TaskID == taskID).FirstOrDefault();
-        //        Assert.AreEqual(knownAnswer, information.title);
-        //    }
-
-        //    // change back
-        //    _revisionManager.UpdateRevisionTask(taskID, title, description, urgency, progress, url);
-        //}
-
     } // end of class
 } // end of namespace
-
-
-
-
-
-
-
-
-
-
-
-//[Test]
-//public void AddingAtaskToArevisionIncreasesNumberOfTasksByOne_WITHINSERTONOFF()
-//{
-
-//    // to be able to insert your own id
-
-//    //"Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Monokayu; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
-//    //using (var command = new SqlCommand
-//    //    (
-//    //        "SET IDENTITY_INSERT dbo.Users OFF",
-//    //        connection
-//    //    )
-//    //)
-//    //{
-//    //    int affected = command.ExecuteNonQuery();
-//    //}
-
-//    // create test data
-//    using (var db = new MonokayuDbContext())
-//    {
-//        // Generate test user
-//        db.Add(new User() { UserID = _userIdToTest, firstName = "Lorenzo0", lastName = "Bulosan", securityLevel = 0, password = "12345678" });
-//        // Generate project for user
-//        db.Add(new Project() { ProjectID = _projectIdToTest, projectName = "project1", UserID = _userIdToTest });
-//        // Generate Revision for project
-//        db.Add(new Revision() { RevisionID = _revisionIdToTest, deadline = new DateTime(), ProjectID = _projectIdToTest });
-
-//        db.SaveChanges();
-//    }
-
-//    // to be able to insert your own id
-//    //using (var command = new SqlCommand
-//    //    (
-//    //        "SET IDENTITY_INSERT dbo.Users ON",
-//    //        connection
-//    //    )
-//    //)
-//    //{
-//    //    int affected = command.ExecuteNonQuery();
-//    //}
-
-//    // testing
-//    using (var db = new MonokayuDbContext())
-//    {
-//        var numberOfTasksBefore = db.RevisionTasks.Count();
-//        _revisionManager.AddTaskToRevision(_revisionIdToTest, "Testing title", "Description test", 3, 0, "empty url");
-//        var numberOfTasksAfter = db.RevisionTasks.Count();
-
-//        Assert.AreEqual(numberOfTasksBefore + 1, numberOfTasksAfter);
-//    }
-//}
